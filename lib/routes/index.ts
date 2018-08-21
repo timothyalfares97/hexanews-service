29058d86f357c702ff2de059121518c93d1c8b86
timothyalfares <timothy_alfares@yahoo.com>
2018-08-22 08:47:38 +1000

    HEX-002 Basic CRUD Implementation

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -2,12 +2,44 @@
 import { Request, Response } from "express"
 
 export class Routes {       
-    public routes(app): void {          
-        app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })               
-    }
+  public routes(app): void {          
+    app.route('/')
+    .get((req: Request, res: Response) => {            
+      res.status(200).send({
+        message: 'System checking: healthy'
+      })
+    })
+        
+    app.route('/article') 
+    .get((req: Request, res: Response) => {        
+      res.status(200).send({
+        message: 'Get all articles'
+      })
+    })
+
+    .post((req: Request, res: Response) => {    
+      res.status(200).send({
+        message: 'Add new article'
+      })
+    })
+
+    app.route('/article/:articleId')
+    .get((req: Request, res: Response) => {          
+      res.status(200).send({
+        message: 'Get an article'
+      })
+    })
+
+    .put((req: Request, res: Response) => {          
+      res.status(200).send({
+        message: 'Update an article'
+      })
+    })
+
+    .delete((req: Request, res: Response) => {        
+      res.status(200).send({
+        message: 'Delete an article'
+      })
+    })
+  }
 }
