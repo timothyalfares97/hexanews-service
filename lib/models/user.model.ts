5627e21c0fe0435001908a48ec1cdde9354fa00b
timothyalfares <timothy_alfares@yahoo.com>
2018-08-26 11:22:41 +1000

    Add bcrypt library, hash password before going through to db

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,19 +1,41 @@
 import * as mongoose from 'mongoose'
+import * as bcrypt from 'bcrypt'
 
 const Schema = mongoose.Schema
+const saltFactor = 10
 
 export const UserSchema = new Schema({
   email: {
     type: String,
+    required: true,
+    unique: true
   },
   password: {
     type: String,
+    required: true
   },
   name: {
     type: String,
   },
   created_date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    required: true
+  }
+})
+
+UserSchema.pre('save', async function(next) {
+  const user = this
+  if (this.isModified('password')) {
+    try {
+      const salt = await bcrypt.genSalt(saltFactor)
+      const hash = await bcrypt.hash(user.password, salt)
+      user.password = hash
+      next()
+    } catch (err) {
+      return next(err)
+    }
+  } else {
+    return next()
   }
 })
