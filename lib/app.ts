4400557984dd10e139b88e9f6f3b39daac788239
audwinoyong <audwinoyong@live.com>
2018-09-13 00:21:06 +1000

    Add json middleware limit (max request body size)

--- lib/app.ts
+++ lib/app.ts
@@ -25,7 +25,7 @@ class App {
 
   private config(): void {
     this.app.use(cors())
-    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.json({ limit: '5mb' }))
     this.app.use(bodyParser.urlencoded({ extended: false }))
     this.app.use(helmet())
   }
